a64fe7f62a9130d97286b183fe2cf4db576d8c5c
Medhat Mohamed <matouk7@outlook.com>
2023-02-04 16:16:36 +0200

    Creat Bank class

---
+++ Simple-Bank-System/app.py
@@ -0,0 +1,40 @@
+class BankAccount():
+    # Class variables
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+    
+    # Class methods
+    # Method to deposit money
+    def deposit(self, amount):
+        self.balance += amount
+        print("You have deposited", amount, "in your account.")
+    # Method to withdraw money
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            print("You have withdrawn", amount, "from your account.")
+        else:
+            print("Insufficient balance.")
+    # Method to display account details
+    def display(self):
+        print("Name:", self.name)
+        print("Balance:", self.balance)
+    # Method to display account balance
+    def balance(self):
+        print("Balance:", self.balance)
+    
+# Create an object of the class
+account = BankAccount("John", 1000)
+# Display account details
+account.display()
+# Deposit money
+account.deposit(500)
+# Display account balance
+account.balance()
+# Withdraw money
+account.withdraw(2000)
+# Display account details
+account.display()
+# Display account balance
+account.balance()
